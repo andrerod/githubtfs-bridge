29865043ffe51ea3236d751b50c4d9c1931cb1e2
Andre Rodrigues <andrerod@microsoft.com>
2013-03-08 06:57:29 -0800

    Adding service project

---
+++ GithubTFSBridgeService/GithubTFSBridge.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace GithubTFSBridgeService
+{
+    public static class GithubTFSBridge
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
