bc92961fcb4bc8ca27abfaf3eea1cee1930e24ec
Andre Rodrigues <andrerod@microsoft.com>
2013-03-08 13:26:31 -0800

    Fix

--- GithubTFSBridge/Program.cs
+++ GithubTFSBridge/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using GithubClient;
+using GithubClient.Model;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
@@ -11,22 +13,46 @@ namespace ConsoleApplication1
         static void Main()
         {
             var githubChannel = GithubClient.GithubClient.CreateChannel(ConfigurationManager.AppSettings["GithubUsername"], ConfigurationManager.AppSettings["GithubPassword"]);
-            var issues = githubChannel.GetIssuesFromRepo("WindowsAzure", "azure-sdk-tools");
+            
+            /*
+            var issues = githubChannel.GetIssuesFromRepo("andrerod", "github-tfs-bridge");
             foreach (var issue in issues)
             {
                 Console.WriteLine(issue.Title);
             }
+            */
 
-                    // Get All Team Projects
+            // Get All Team Projects
             var tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri("http://vstfrd.dns.corp.microsoft.com:8080"));
             var wiStore = tfs.GetService<WorkItemStore>();
 
-            foreach (var project in wiStore.Projects)
-            {
-                Console.WriteLine(project.ToString());
-            }
+            var project = wiStore.Projects.Cast<Project>().FirstOrDefault(p =>
+                p.Name.Equals("RD", StringComparison.InvariantCultureIgnoreCase));
 
+            QueryFolder sharedQueriesFolder = project.QueryHierarchy["Shared Queries"] as QueryFolder;
+            QueryFolder kuduFolder = sharedQueriesFolder["Kudu"] as QueryFolder;
+
+            QueryDefinition query = kuduFolder["Portal - Kudu Future"] as QueryDefinition;
+            if (query != null)
+            {
+                var workItems = wiStore.Query(query.QueryText.Replace("@project", "'" + query.Project.Name + "'"));
+                foreach (WorkItem workItem in workItems)
+                {
+                    githubChannel.CreateIssue("andrerod", "testrepo", CreateGithubIssue(workItem));
+                }
+            }
+            
             Console.ReadLine();
+        }
+
+        public static GithubIssue CreateGithubIssue(WorkItem workItem)
+        {
+            GithubIssue githubIssue = new GithubIssue
+            {
+                Title = workItem.Title
+            };
+
+            return githubIssue;
         }
     }
 }
